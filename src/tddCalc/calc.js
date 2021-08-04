6fdfa1d8ff427a9376ca87794604abefe5f4df38
eiji.ienaga <eiji.ienaga@gmail.com>
2021-08-05 08:32:42 +0900

    演習箇所を削除

--- src/tddCalc/calc.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable class-methods-use-this */
-class Calc {
-  add(augend, addend) {
-    return augend + addend;
-  }
-}
-
-module.exports = Calc;
--- src/tddCalc/calc.test.js
+++ src/tddCalc/calc.test.js
@@ -1,14 +1,7 @@
-const Calc = require('./Calc');
-
 describe('Calcについて', () => {
-  test('お試し', () => {
-    expect(1 + 2).toBe(3);
-  });
-  test('add 足し算が計算できる', () => {
-    const calc = new Calc();
-    expect(calc.add(1, 2)).toBe(3);
-  });
+  test.todo('add 足し算が計算できる');
   test.todo('sub 引き算が計算できる');
   test.todo('mul 掛け算が計算できる');
   test.todo('div 割り算が計算できる');
+  test.todo('ゼロ割り算の計算はあとで考える');
 });

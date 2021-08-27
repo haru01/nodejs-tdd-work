0311f06c2c1ea6f57536889baf6ef12f68d73619
eiji.ienaga <eiji.ienaga@gmail.com>
2021-08-27 15:16:15 +0900

    TDDの実演スタートをつくった

--- src/first.test.js
+++ src/first.test.js
@@ -5,3 +5,20 @@ function add(a, b) {
 test('足し算できること', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+describe('Stack#size.スタックに積まれている値の数を取得する', () => {
+  test.todo('からの場合は ０ であること');
+  test.todo('複数回Push したら回数分 であること');
+});
+
+describe('Stack#push.スタックの一番上に積む', () => {
+  test.todo('Pushしていない場合');
+  test.todo('1回Pushした場合');
+  test.todo('複数回Pushした場合');
+});
+
+describe('Stack#pop.スタックの一番上の値を返し、取り除く', () => {
+  test.todo('上の要素が取れること');
+  test.todo('上の要素が取り除かれること');
+  test.todo('スタックが空の場合EmptyErrorが発生');
+});

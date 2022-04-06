6acbc4029e85bd371be2a1c65e662456fefe4d4c
eiji.ienaga <eiji.ienaga@gmail.com>
2022-04-06 12:51:00 +0900

    add FizzBuzz

---
+++ src/fizzBuzz/FizzBuzz.test.js
@@ -0,0 +1,8 @@
+describe('FizzBuzzについて', () => {
+  test.todo('3の倍数ならFizzを返すこと');
+  test.todo('5の倍数ならBuzzを返すこと');
+  test.todo('3と5の倍数ならFizzBuzzを返すこと');
+  test.todo('上記以外（３の倍数でも５の倍数でもない）は数字を返すこと');
+  test.todo('1から100でFizzBuzzが表示されること');
+  test.todo('気になるテストケース....');
+});
